b15462f09ad535597ff6257d69ff3208f55d79a0
johnanairn <johnanairn@b5a0aab0-f42e-11de-82c3-25da7dca7fdf>
2009-12-30 19:56:05 +0000

    Color wells for pplot elements

--- Visualization/src/Interface/NFMVPrefs.java
+++ Visualization/src/Interface/NFMVPrefs.java
@@ -64,6 +64,17 @@ public class NFMVPrefs extends JFrame implements ActionListener
 	public static String NumContoursKey="NumberOfPlotContours";
 	public static int NumContoursDef=0;
 	
+	// plot colors
+	public static String backColorKey="Background Color";
+	public static Color backColorDef=new Color(84,89,109);  		// 0.329f,0.349f,0.427f
+	public static String meshLineColorKey="Mesh Line Color";
+	public static Color meshLineColorDef=new Color(191,191,191);	// 0.75f,0.75f,0.75f
+	public static String meshNodesColorKey="Nodal Point Color";
+	public static Color meshNodesColorDef=new Color(212,212,212);	// 0.83f,0.83f,0.83f
+	public static String textColorKey="Text Label Color";
+	public static Color textColorDef=new Color(255,255,255);		// 1.f,1.f,1.f
+
+	
 	private JTextField mpmCodePath=new JTextField();
 	private JTextField mpmDTDPath=new JTextField();
 	private JTextField feaCodePath=new JTextField();
@@ -88,7 +99,7 @@ public class NFMVPrefs extends JFrame implements ActionListener
 		
 		JTabbedPane tabbedPane = new JTabbedPane();
 
-		// code preferences
+		// code preferences pane -------------------------------------
 		JPanel panel1 = new JPanel();
 		
 		panel1.add(filePathPanel("MPM Code",mpmCodePath,prefs.get(NairnMPMKey,NairnMPMDef),
@@ -106,77 +117,148 @@ public class NFMVPrefs extends JFrame implements ActionListener
 		
 		tabbedPane.addTab("Code",null,panel1,"Settings for running calculations");
 
-		// color preferences
+		// color preferences pane ----------------------------------------
 		JPanel panel2 = new JPanel();
 		tabbedPane.addTab("Colors",null,panel2,"Select colors used in plots");
 
 		GridBagLayout gridbag = new GridBagLayout();
-		GridBagConstraints c = new GridBagConstraints();
+		GridBagConstraints pc = new GridBagConstraints();		// for the pane
 		panel2.setLayout(gridbag);
 		
-		// spectrum groups title -------------------------------------------
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridx=0;
-		c.gridwidth = 1;
-		c.weightx = 1.0;
+		// spectrum box
 		GridBagLayout spectrumBag = new GridBagLayout();
 		JNBoxPanel spectrumPan=new JNBoxPanel("Color Plot Spectrum",spectrumBag);
+		GridBagConstraints c = new GridBagConstraints();		// for the box panel
 		
 		JLabel section=new JLabel("Type: ");
 		c.fill = GridBagConstraints.NONE;
-		c.insets=new Insets(6,6,1,0);			// tlbr
 		c.anchor = GridBagConstraints.WEST;
 		c.gridx=0;
-		c.weightx = 0.0;
 		c.gridwidth = 1;
+		c.weightx = 0.0;
+		c.insets=new Insets(6,6,1,0);			// tlbr
 		spectrumBag.setConstraints(section,c);
 		spectrumPan.add(section);
 		
-		c.insets=new Insets(6,3,1,6);			// tlbr
-		c.anchor = GridBagConstraints.WEST;
-		c.gridx=1;
-		c.weightx = 1.0;
-		c.gridwidth = 2;
 		String [] lines={"Rainbow 1 (Blue to Cyan)","Rainbow 2 (Blue to Red)","Rainbow 3 (Purple to Orange)","Grayscale (Black to White)","Cool Diverging (Blue to Red)"};
 		spectrumBox=new JComboBox(lines);
 		spectrumBox.setSelectedIndex(ColorPicker.getSpectrumType());
 		spectrumBox.addActionListener(this);
 		spectrumBox.setFocusable(false);
 		spectrumBox.setToolTipText("Select one of the available spectra for coloring the plots.");
+		c.insets=new Insets(6,3,1,6);			// tlbr
+		c.gridx=1;
+		c.weightx = 1.0;
 		spectrumBag.setConstraints(spectrumBox,c);
 		spectrumPan.add(spectrumBox);
 
 		JLabel section1=new JLabel("Contours: ");
-		c.fill = GridBagConstraints.NONE;
 		c.insets=new Insets(6,6,1,0);			// tlbr
-		c.anchor = GridBagConstraints.WEST;
 		c.gridx=0;
 		c.weightx = 0.0;
-		c.gridwidth = 1;
 		spectrumBag.setConstraints(section1,c);
 		spectrumPan.add(section1);
 
-		c.insets=new Insets(6,3,1,6);			// tlbr
-		c.anchor = GridBagConstraints.WEST;
-		c.gridx=1;
 		numContours.setText(""+ColorPicker.getNumberOfContours());
 		numContours.setEditable(true);
 		numContours.setColumns(5);
 		numContours.setToolTipText("Enter 1 for continuous colors or >1 for number of discrete colors and hit return or enter");
 		numContours.setActionCommand("change contours");
 		numContours.addActionListener(this);
+		c.insets=new Insets(6,3,1,6);			// tlbr
+		c.gridx=1;
+		c.weightx = 1.0;
 		spectrumBag.setConstraints(numContours,c);
 		spectrumPan.add(numContours);
 		
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx=0;
-		c.insets=new Insets(1,6,1,6);			// tlbr
-		c.anchor = GridBagConstraints.CENTER;
+		c.gridwidth = 1;
 		c.weightx = 1.0;
-		gridbag.setConstraints(spectrumPan,c);
+
+		pc.fill = GridBagConstraints.HORIZONTAL;
+		pc.gridx=0;
+		pc.gridwidth = 1;
+		pc.weightx = 1.0;
+		pc.insets=new Insets(1,6,1,6);			// tlbr
+		pc.anchor = GridBagConstraints.CENTER;
+		gridbag.setConstraints(spectrumPan,pc);
 		panel2.add(spectrumPan);
 
-		// FEA and MPM preferences
+		// plot element color wells box
+		GridBagLayout wellsBag = new GridBagLayout();
+		JNBoxPanel wellsPan=new JNBoxPanel("Plot Elements",wellsBag);
+		GridBagConstraints ec = new GridBagConstraints();		// for the plot element box panel
+		
+		JLabel elab1=new JLabel("Background: ");
+		ec.fill = GridBagConstraints.NONE;
+		ec.anchor = GridBagConstraints.EAST;
+		ec.insets=new Insets(10,0,0,0);			// tlbr
+		ec.gridx=0;
+		ec.weightx = 0.5;
+		ec.gridwidth=1;
+		wellsBag.setConstraints(elab1,ec);
+		wellsPan.add(elab1);
+		
+		JNColorWell bgWell=new JNColorWell(getPrefColor(backColorKey,backColorDef));
+		bgWell.addActionListener(this,"bgColor","Choose Background Color");
+		ec.anchor = GridBagConstraints.WEST;
+		ec.gridx=1;
+		ec.weightx = 1.0;
+		wellsBag.setConstraints(bgWell,ec);
+		wellsPan.add(bgWell);
+		
+		JLabel elab2=new JLabel("Mesh Lines: ");
+		ec.anchor = GridBagConstraints.EAST;
+		ec.gridx=2;
+		ec.weightx = 0.5;
+		wellsBag.setConstraints(elab2,ec);
+		wellsPan.add(elab2);
+		
+		JNColorWell mlWell=new JNColorWell(getPrefColor(meshLineColorKey,meshLineColorDef));
+		mlWell.addActionListener(this,"mlColor","Choose Mesh Line Color");
+		ec.anchor = GridBagConstraints.WEST;
+		ec.gridx=3;
+		ec.weightx = 1.0;
+		wellsBag.setConstraints(mlWell,ec);
+		wellsPan.add(mlWell);
+		
+		JLabel elab3=new JLabel("Labels: ");
+		ec.insets=new Insets(10,0,6,0);			// tlbr
+		ec.anchor = GridBagConstraints.EAST;
+		ec.gridx=0;
+		ec.weightx = 0.5;
+		ec.gridwidth=1;
+		wellsBag.setConstraints(elab3,ec);
+		wellsPan.add(elab3);
+		
+		JNColorWell labWell=new JNColorWell(getPrefColor(textColorKey,textColorDef));
+		labWell.addActionListener(this,"labColor","Choose Color for Text Labels");
+		ec.anchor = GridBagConstraints.WEST;
+		ec.gridx=1;
+		ec.weightx = 1.0;
+		wellsBag.setConstraints(labWell,ec);
+		wellsPan.add(labWell);
+		
+		JLabel elab4=new JLabel("Nodal Points: ");
+		ec.anchor = GridBagConstraints.EAST;
+		ec.gridx=2;
+		ec.weightx = 0.5;
+		wellsBag.setConstraints(elab4,ec);
+		wellsPan.add(elab4);
+		
+		JNColorWell npWell=new JNColorWell(getPrefColor(meshNodesColorKey,meshNodesColorDef));
+		npWell.addActionListener(this,"npColor","Choose Nodal Point Color");
+		ec.anchor = GridBagConstraints.WEST;
+		ec.gridx=3;
+		ec.weightx = 1.0;
+		wellsBag.setConstraints(npWell,ec);
+		wellsPan.add(npWell);
+
+		gridbag.setConstraints(wellsPan,pc);
+		panel2.add(wellsPan);
+	
+		// FEA and MPM preferences pane -----------------------------------------
 		JPanel panel3 = new JPanel();
 		tabbedPane.addTab("FEA and MPM",null,panel3,"FEA and MPM plot settings");
 		
@@ -277,6 +359,30 @@ public class NFMVPrefs extends JFrame implements ActionListener
 		{	prefs.putBoolean(NairnFEAValidateKey,validateFEA.isSelected());
 		}
 		
+		else if(theCmd.equals("bgColor"))
+		{	JNColorWell cWell=(JNColorWell)e.getSource();
+			setPrefColor(backColorKey,cWell.getWellColor());
+			return;
+		}
+		
+		else if(theCmd.equals("labColor"))
+		{	JNColorWell cWell=(JNColorWell)e.getSource();
+			setPrefColor(textColorKey,cWell.getWellColor());
+			return;
+		}
+		
+		else if(theCmd.equals("mlColor"))
+		{	JNColorWell cWell=(JNColorWell)e.getSource();
+			setPrefColor(meshLineColorKey,cWell.getWellColor());
+			return;
+		}
+		
+		else if(theCmd.equals("npColor"))
+		{	JNColorWell cWell=(JNColorWell)e.getSource();
+			setPrefColor(meshNodesColorKey,cWell.getWellColor());
+			return;
+		}
+		
 		else if(theCmd.equals("FEA DTD File"))
 		{	int result=chooser.showOpenDialog(this);
 			if(result==JFileChooser.CANCEL_OPTION) return;
@@ -344,6 +450,23 @@ public class NFMVPrefs extends JFrame implements ActionListener
 	public static Dimension getFrameSize(NFMVFrame f)
 	{	return new Dimension(prefs.getInt(f.frameWidthKey,f.frameWidthDef),
 				prefs.getInt(f.frameHeightKey,f.frameHeightDef));
+	}
+	
+	// set/get color using three float preferences
+	public void setPrefColor(String colorKey,Color setColor)
+	{	prefs.putInt(colorKey+" Red", setColor.getRed());
+		prefs.putInt(colorKey+" Green", setColor.getGreen());
+		prefs.putInt(colorKey+" Blue", setColor.getBlue());
+	}
+	public static Color getPrefColor(String colorKey,Color colorDef)
+	{	try
+		{	return new Color(prefs.getInt(colorKey+" Red", colorDef.getRed()),
+					prefs.getInt(colorKey+" Green", colorDef.getGreen()),
+					prefs.getInt(colorKey+" Blue", colorDef.getBlue()));
+		}
+		catch(Exception ce)
+		{	return new Color(128,128,128);
+		}
 	}
 
 	// set a chooser to current work space
