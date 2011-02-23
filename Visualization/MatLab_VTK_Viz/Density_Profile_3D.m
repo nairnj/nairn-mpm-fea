4a396ad6a6b3127e175341f75dc494896cfb43e6
johnanairn <johnanairn@b5a0aab0-f42e-11de-82c3-25da7dca7fdf>
2011-02-23 01:23:55 +0000

    MatLab stuff

--- Visualization/MatLab_VTK_Viz/Density_Profile_3D.m
+++ Visualization/MatLab_VTK_Viz/Density_Profile_3D.m
@@ -77,27 +77,68 @@ while (hasLimits==0)
 end
 
 % switch ranges if needed
+if (rng1(1)>rng1(2))
+    rng1 = [rng1(2) rng1(1)];
+end
+if (rng2(1)>rng2(2))
+    rng2 = [rng2(2) rng2(1)];
+end
 
 % integrate area along axis
+if(axis==1)
+    lim1 = [max(1,fix((rng1(1)-origin(2))/spacing(2))):...
+                 min(dims(2),fix((rng1(2)-origin(2))/spacing(2)))];
+    lim2 = [max(1,fix((rng2(1)-origin(3))/spacing(3))):...
+                 min(dims(3),fix((rng2(2)-origin(3))/spacing(3)))];
+else
+    lim1 = [max(1,fix((rng1(1)-origin(1))/spacing(1))):...
+                 min(dims(1),fix((rng1(2)-origin(1))/spacing(1)))];
+    if(axis==3)
+        lim2 = [max(1,fix((rng2(1)-origin(2))/spacing(2))):...
+                 min(dims(2),fix((rng2(2)-origin(2))/spacing(2)))];
+    else
+        lim2 = [max(1,fix((rng2(1)-origin(3))/spacing(3))):...
+                 min(dims(3),fix((rng2(2)-origin(3))/spacing(3)))];
+    end
+end
+
+tdat = [];
+ddat = [];
+if(axis==1)
+    for i = 1:dims(1)
+        tdat(i) = origin(1)+(i-1)*spacing(1);
+        dplane = squeeze(qdata(i,lim1,lim2));
+        ddat(i) = sum(sum(dplane));
+    end
+elseif(axis==2)
+    for i = 1:dims(2)
+        tdat(i) = origin(2)+(i-1)*spacing(2);
+        dplane = squeeze(qdata(lim1,i,lim2));
+        ddat(i) = sum(sum(dplane));
+    end
+else
+    for i = 1:dims(3)
+        tdat(i) = origin(3)+(i-1)*spacing(3);
+        dplane = squeeze(qdata(lim1,lim2,i));
+        ddat(i) = sum(sum(dplane));
+    end
+end
+
+% scale to denity in g/cm^3
+s1 = size(lim1);
+s2 = size(lim2);
+cellVolume = (s1(2)-1)*(s2(2)-1)*spacing(1)*spacing(2)*spacing(3)/1000.;
+ddat = ddat/cellVolume;
+
+% print average density
+ds = size(ddat);
+avgDensity = sum(ddat)/ds(2);
 
 % plot the results
-
-% point coordinates
-VTKfields = {VTKheader(:).parname};
-origin = VTKheader(strcmpi('ORIGIN',VTKfields)).parval;
-spacing = VTKheader(strcmpi('SPACING',VTKfields)).parval;
-dims = size(qdata);
-xloc = [origin(1):spacing(1):(origin(1)+(dims(1)-1)*spacing(1))];
-yloc = [origin(2):spacing(2):(origin(2)+(dims(2)-1)*spacing(2))];
-zloc = [origin(3):spacing(3):(origin(3)+(dims(3)-1)*spacing(3))];
-[X,Y,Z] = ndgrid(xloc,yloc,zloc);
-
-% vector list of points
-npts = dims(1)*dims(2)*dims(3);
-qv = reshape(qdata,npts,1);
-xv = reshape(X,npts,1);
-yv = reshape(Y,npts,1);
-zv = reshape(Z,npts,1);
-
-%figure(1); scatter3(xv,yv,zv,3,qv,'filled')
+figure(1);
+p = plot(tdat,ddat);
+xlabel('Position (mm)')
+ylabel('Density (g/cm^3)')
+title('MPM Density Profile')
+text(tdat(fix(ds(2)/2)),.05,['avg = ' num2str(avgDensity)])
 
