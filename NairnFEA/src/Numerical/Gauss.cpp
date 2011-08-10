e3a6e732c54be348897758355853768ad68861a9
johnanairn <johnanairn@b5a0aab0-f42e-11de-82c3-25da7dca7fdf>
2011-08-10 22:16:41 +0000

    auto commit NairnMPM changes

--- NairnFEA/src/Numerical/Gauss.cpp
+++ NairnFEA/src/Numerical/Gauss.cpp
@@ -16,7 +16,8 @@ int gelbnd(double **,int,int,double *,double *,int);
     band storage format such that aij=a[j-i+1][i] and
     storage includes only elements with
             1 <= j-i+1 <= nband
-    where nband is bandwidth of the matrix
+    where nband is bandwidth of the matrix. In other words
+            a[k][i] = ai,i+k-1
 
     Input parameters
         a: matrix in band symmetric storage
@@ -36,6 +37,8 @@ int gelbnd(double **,int,int,double *,double *,int);
     Method
         Gaussian elimination with no pivoting which means
     diagonal elements better be OK
+ 
+	Some personal notes found at JANOSU-4-197
 ***********************************************************/
 
 int gelbnd(double **a,int n,int nband,double *r,double *work,int iflag)
@@ -54,6 +57,11 @@ int gelbnd(double **a,int n,int nband,double *r,double *work,int iflag)
     }
 
     // If iflag=0 then do matrix reduction
+	// Reducing matrix a into a = LU where
+	//   L is lower diagonal matrix (with ones on the diagonal)
+	//   U is upper diagonal matrix
+	// Because a is symmetric, only need to find U and it replaces
+	//	the a elements as it is calculated
     if(iflag==0)
     {	// Make copy of the diagonal elements
         for(i=1;i<=n;i++) work[i]=a[1][i];
@@ -65,58 +73,80 @@ int gelbnd(double **a,int n,int nband,double *r,double *work,int iflag)
             jend=fmin(nband,n-imin1);
 
             // Check for over reduced or bad matrix 
-            temp=a[1][i];
+            temp=a[1][i];			// Now Uii since row i has been reduced
             if(temp==0.)
                 return 1;
-            else
-            {	temp2=temp/work[i];
+            else if(work[i]!=0.)
+			{	// compare to initial diagonal element if possible
+            	temp2=temp/work[i];
                 if(temp2<0.) temp2=-temp2;
                 if(temp2<1e-12) ierr=-1;
             }
             
+			// Let k = j+i-1 and m = k+ind-1 = k+j2-j
+			//		and then subtract (Uik/Uii)*Uim from akm to end
+			//      looping k = i+1 to end, m = k to end
+			// In other words: Ukm = akm - Sum_{i=1,k-1} (Uik/Uii)*Uim
             for(j=2;j<=jend;j++)
-            {	factor=a[j][i];
+            {	factor=a[j][i];			// Now Uik since row i has been reduced
                 if(factor!=0.)
-                {   scale=factor/temp;
+				{	scale=factor/temp;
                     k=j+imin1;
                     ind=0;
                     for(j2=j;j2<=jend;j2++)
                     {	ind=ind+1;
-                        factor=a[j2][i];
+                        factor=a[j2][i];		// Now Ui,j2+i-1 since row i has been reduced
                         if(factor!=0.)
                             a[ind][k]-=scale*factor;
                     }
-                    a[j][i]=scale;
+                    a[j][i]=scale;		// Now Uik/Uii = Lki
                 }
             }
+			
+			// Elements through a[][i+1] now fully reduced to row i+1 or U
         }
+		
+		// As reduced, a[1][i] is diagonal element Uii and a[j][i] is Li+j-1,i
     }
 	   
     // Forward reduction of the right side of the equation
+	// Solve Ly = r
     for(i=1;i<=n;i++)
     {	imin1=i-1;
         jend=fmin(nband,n-imin1);
+		
+		// Let k = i+j-1, then k from i+1 to end
         for(j=2;j<=jend;j++)
-        {   factor=a[j][i];
+        {   factor=a[j][i];			// Lki or loop Li+1,i to end
             if(factor!=0.)
             {	ind=imin1+j;
                 r[ind]-=factor*r[i];
             }
         }
+		
+		// r[i+1] has been converted to yi
+		
+		// No longer need yi in r[i], so scale now by diagonal r[i] = yi/Uii
         r[i]/=a[1][i];
     }
 
-    /* Solve for unknowns by back substitution and put the
-                    answer back into r */
+    // Solve for unknowns by back substitution and put the answer back into r
+	// Solve U x = y
+	// First xn = yn/Unn = rn from above forward reduction
+	// Find rest xi = ri - Sum_{k=i+1,n} Lki xk
     for(i=n-1;i>=1;i--)
     {	imin1=i-1;
         jend=fmin(nband,n-imin1);
         sum=0.;
+		
+		// Let k=i+j-1 and loop k=i+1 to end
         for(j=2;j<=jend;j++)
-        {   factor=a[j][i];
+        {   factor=a[j][i];		// From decomposition is Lki
             if(factor!=0.)
-                sum+=factor*r[imin1+j];
+                sum+=factor*r[imin1+j];			// summand Lki*xk
         }
+		
+		// subtract sum to get xi
         r[i]-=sum;
     }
     
