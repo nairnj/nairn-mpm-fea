dd96e33f904a96eb3023ef8322514da0ea20619b
johnanairn <johnanairn@b5a0aab0-f42e-11de-82c3-25da7dca7fdf>
2013-04-26 00:04:22 +0000

    zero zDeriv[] as might be needed on some computers

--- trunk/Common/System/svninfo.cpp
+++ trunk/Common/System/svninfo.cpp
@@ -1,1 +1,1 @@
-const char *svninfo = "URL: https://nairn-mpm-fea.googlecode.com/svn/trunk, Revision: 763";
+const char *svninfo = "URL: https://nairn-mpm-fea.googlecode.com/svn/trunk, Revision: 764";
