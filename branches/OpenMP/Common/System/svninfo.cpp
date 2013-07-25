6d6825e182def28517ce3f1c9e64ff5e1a3e7162
johnanairn <johnanairn@b5a0aab0-f42e-11de-82c3-25da7dca7fdf>
2013-07-25 13:12:19 +0000

    auto commit NairnMPM changes

--- branches/OpenMP/Common/System/svninfo.cpp
+++ branches/OpenMP/Common/System/svninfo.cpp
@@ -1,1 +1,1 @@
-const char *svninfo = "URL: https://nairn-mpm-fea.googlecode.com/svn/branches/OpenMP, Revision: 798";
+const char *svninfo = "URL: https://nairn-mpm-fea.googlecode.com/svn/branches/OpenMP, Revision: 799";
