2972d291a9c70f50310b6158169dfc6e7b47fb88
johnanairn <johnanairn@b5a0aab0-f42e-11de-82c3-25da7dca7fdf>
2013-08-30 21:44:06 +0000

    minor edits, it any

--- branches/OpenMP/Common/System/svninfo.cpp
+++ branches/OpenMP/Common/System/svninfo.cpp
@@ -1,1 +1,1 @@
-const char *svninfo = "URL: https://nairn-mpm-fea.googlecode.com/svn/branches/OpenMP, Revision: 821";
+const char *svninfo = "URL: https://nairn-mpm-fea.googlecode.com/svn/branches/OpenMP, Revision: 832";
