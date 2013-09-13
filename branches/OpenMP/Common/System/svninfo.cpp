ebb6e9810633561f00c874b3bf83e038d7906d07
johnanairn <johnanairn@b5a0aab0-f42e-11de-82c3-25da7dca7fdf>
2013-09-13 16:13:48 +0000

    synch with java

--- branches/OpenMP/Common/System/svninfo.cpp
+++ branches/OpenMP/Common/System/svninfo.cpp
@@ -1,1 +1,1 @@
-const char *svninfo = "URL: https://nairn-mpm-fea.googlecode.com/svn/branches/OpenMP, Revision: 848";
+const char *svninfo = "URL: https://nairn-mpm-fea.googlecode.com/svn/branches/OpenMP, Revision: 850";
