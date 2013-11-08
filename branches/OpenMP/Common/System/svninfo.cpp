74cea1e8497d626898b0ac5a7376bf566354082c
johnanairn <johnanairn@b5a0aab0-f42e-11de-82c3-25da7dca7fdf>
2013-11-08 00:58:56 +0000

    crack initialization back to parallel

--- branches/OpenMP/Common/System/svninfo.cpp
+++ branches/OpenMP/Common/System/svninfo.cpp
@@ -1,1 +1,1 @@
-const char *svninfo = "URL: https://nairn-mpm-fea.googlecode.com/svn/branches/OpenMP, Revision: 888";
+const char *svninfo = "URL: https://nairn-mpm-fea.googlecode.com/svn/branches/OpenMP, Revision: 889";
