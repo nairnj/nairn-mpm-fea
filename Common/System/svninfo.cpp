2ab7acc785189edad29d7f82b08d22d01e5d54f8
johnanairn <johnanairn@b5a0aab0-f42e-11de-82c3-25da7dca7fdf>
2014-01-23 17:41:20 +0000

    replaced some new double[] with mallocs in NairnFEA mainly so it can pinpoint memoery issues when they arise without needed catch blocks

--- Common/System/svninfo.cpp
+++ Common/System/svninfo.cpp
@@ -1,1 +1,1 @@
-const char *svninfo = "URL: https://johnanairn@nairn-mpm-fea.googlecode.com/svn/trunk, Revision: 957";
+const char *svninfo = "URL: https://johnanairn@nairn-mpm-fea.googlecode.com/svn/trunk, Revision: 958";
