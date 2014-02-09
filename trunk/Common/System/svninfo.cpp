ec3ef5d0cf7d588b5e5451a27af19f57cd9649f1
johnanairn <johnanairn@b5a0aab0-f42e-11de-82c3-25da7dca7fdf>
2014-02-09 23:33:36 +0000

    Final commit before create release 5.0 tagged version

--- trunk/Common/System/svninfo.cpp
+++ trunk/Common/System/svninfo.cpp
@@ -1,1 +1,1 @@
-const char *svninfo = "URL: https://johnanairn@nairn-mpm-fea.googlecode.com/svn/trunk, Revision: 961";
+const char *svninfo = "URL: https://johnanairn@nairn-mpm-fea.googlecode.com/svn/trunk, Revision: 980";
