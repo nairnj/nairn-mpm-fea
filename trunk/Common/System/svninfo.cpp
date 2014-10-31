8d62c9d87a4c98daea0cf50078b86665a894e0b7
johnanairn <johnanairn@b5a0aab0-f42e-11de-82c3-25da7dca7fdf>
2014-10-31 23:51:33 +0000

    auto commit NairnMPM changes

--- trunk/Common/System/svninfo.cpp
+++ trunk/Common/System/svninfo.cpp
@@ -1,1 +1,1 @@
-const char *svninfo = "URL: https://johnanairn@nairn-mpm-fea.googlecode.com/svn/trunk, Revision: 1048";
+const char *svninfo = "URL: https://nairn-mpm-fea.googlecode.com/svn/trunk, Revision: 1049";
