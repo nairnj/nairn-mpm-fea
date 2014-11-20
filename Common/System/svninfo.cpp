59f8c7ba9432b1584163fb20020287a82749596b
johnanairn <johnanairn@b5a0aab0-f42e-11de-82c3-25da7dca7fdf>
2014-11-20 05:29:10 +0000

    Switch Jcountour grid energy default to no

--- Common/System/svninfo.cpp
+++ Common/System/svninfo.cpp
@@ -1,1 +1,1 @@
-const char *svninfo = "URL: https://johnanairn@nairn-mpm-fea.googlecode.com/svn/trunk, Revision: 1069";
+const char *svninfo = "URL: https://johnanairn@nairn-mpm-fea.googlecode.com/svn/trunk, Revision: 1072";
