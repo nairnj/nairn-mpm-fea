f8d92a7cf374c8950e46c46ae19dcf6299f10c73
johnanairn <johnanairn@b5a0aab0-f42e-11de-82c3-25da7dca7fdf>
2015-03-09 17:09:59 +0000

    deprecated command addition

--- trunk/Common/System/svninfo.cpp
+++ trunk/Common/System/svninfo.cpp
@@ -1,1 +1,1 @@
-const char *svninfo = "URL: https://johnanairn@nairn-mpm-fea.googlecode.com/svn/trunk, Revision: 1100";
+const char *svninfo = "URL: https://johnanairn@nairn-mpm-fea.googlecode.com/svn/trunk, Revision: 1101";
