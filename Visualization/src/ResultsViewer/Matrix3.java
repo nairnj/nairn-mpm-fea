e9d159ca9d8cda8f23c0e72e95cd8639e7184fa5
johnanairn <johnanairn@b5a0aab0-f42e-11de-82c3-25da7dca7fdf>
2015-06-01 02:15:35 +0000

    Removed use of Vector3d

--- Visualization/src/ResultsViewer/Matrix3.java
+++ Visualization/src/ResultsViewer/Matrix3.java
@@ -6,7 +6,6 @@
  * Copyright 2015, RSAC Software, All Rights Reserved
  */
 
-import javax.vecmath.Vector3d;
 
 public class Matrix3
 {
@@ -159,9 +158,9 @@ public class Matrix3
 	
 	// Find Eigenvalues of positive definite matrix return in a vector
 	// Assumes all are real and positive (any other will fail)
-	public Vector3d Eigenvalues()
+	public double[] Eigenvalues()
 	{
-		Vector3d lam = new Vector3d();
+		double[] lam = new double[3];
 	    
 	    if(is2D)
 	    {   // solving x^2 + bx + c = 0 (see Numerical Recipes in C, page 156)
@@ -171,14 +170,14 @@ public class Matrix3
 			if(arg<0.)
 			{	// assuming here all matrices are positive definite, which means
 				// a negative value should be interpreted as zero
-				lam.x = -0.5*b;
+				lam[0] = -0.5*b;
 			}
 			else
 			{	arg = Math.sqrt(arg);
-				lam.x = b>0 ? -0.5*(b+arg) : -0.5*(b-arg) ;
+				lam[0] = b>0 ? -0.5*(b+arg) : -0.5*(b-arg) ;
 			}
-	        lam.y = c/lam.x;
-	        lam.z = m[2][2];
+	        lam[1] = c/lam[0];
+	        lam[2] = m[2][2];
 	    }
 	    else
 	    {	double mm, c1, c0;
@@ -208,10 +207,10 @@ public class Matrix3
 			c = sqrt_p*Math.cos(phi);
 			s = (1.0/M_SQRT3)*sqrt_p*Math.sin(phi);
 			
-			lam.y  = (1.0/3.0)*(mm - c);
-			lam.z  = lam.y + s;
-			lam.x  = lam.y + c;
-			lam.y -= s;
+			lam[1]  = (1.0/3.0)*(mm - c);
+			lam[2]  = lam[1] + s;
+			lam[0]  = lam[1] + c;
+			lam[1] -= s;
 	    }
 	    
 	    return lam;
@@ -221,20 +220,19 @@ public class Matrix3
 	// Provide eigenvalues when call in values for 2D, but not needed for 3D
 	// Assumes all are real and positive (any other will fail)
 	// No change to target matrix
-	public Matrix3 Eigenvectors(Vector3d Eigenvals)
+	public Matrix3 Eigenvectors(double[] Eigenvals)
 	{
 		Matrix3 Eigenvecs = new Matrix3();
 		
 	    if(is2D)
-		{	double [] vals = dsyev2(m[0][0],m[0][1],m[1][1],Eigenvals.x,Eigenvals.y);
-			Eigenvals.x = vals[0];
-			Eigenvals.y = vals[1];
+		{	double [] vals = dsyev2(m[0][0],m[0][1],m[1][1],Eigenvals[0],Eigenvals[1]);
+			Eigenvals[0] = vals[0];
+			Eigenvals[1] = vals[1];
 			Eigenvecs.set(vals[2],-vals[3],vals[3],vals[2],1.);
 	    }
 	    
 	    else
 		{	// set to 3D matrix
-			double w[] = new double[3];
 			double Q[][] = new double[3][3];
 			double A[][] = new double[3][3];
 			A[0][0] = m[0][0];
@@ -243,11 +241,8 @@ public class Matrix3
 			A[1][1] = m[1][1];
 			A[1][2] = m[1][2];
 			A[2][2] = m[2][2];
-			dsyevq3(A, Q, w);			// QL - if finds eigenvalues
+			dsyevq3(A, Q, Eigenvals);			// QL - if finds eigenvalues
 			Eigenvecs.set(Q);
-			Eigenvals.x = w[0];
-			Eigenvals.y = w[1];
-			Eigenvals.z = w[2];
 	    }
 	    
 	    return Eigenvecs;
@@ -291,10 +286,10 @@ public class Matrix3
 	    B2.times(B);
 	    
 	    // Eigenvalues of B are lamda^2
-	    Vector3d Eigenvals = B.Eigenvalues();
-	    double lam1 = Math.sqrt(Eigenvals.x);
-	    double lam2 = Math.sqrt(Eigenvals.y);
-	    double lam3 = Math.sqrt(Eigenvals.z);
+	    double[] Eigenvals = B.Eigenvalues();
+	    double lam1 = Math.sqrt(Eigenvals[0]);
+	    double lam2 = Math.sqrt(Eigenvals[1]);
+	    double lam3 = Math.sqrt(Eigenvals[2]);
 	    
 	    // invariants of V
 	    double i1 = lam1+lam2+lam3;
@@ -440,214 +435,6 @@ public class Matrix3
 		return response;
 	}
 
-	// ----------------------------------------------------------------------------
-	// Calculates the eigenvalues and normalized eigenvectors of a symmetric 3x3
-	// matrix A using Cardano's method for the eigenvalues and an analytical
-	// method based on vector cross products for the eigenvectors.
-	// Only the diagonal and upper triangular parts of A need to contain meaningful
-	// values. However, all of A may be used as temporary storage and may hence be
-	// destroyed.
-	// ----------------------------------------------------------------------------
-	// Parameters:
-	//   A: The symmetric input matrix
-	//   Q: Storage buffer for eigenvectors
-	//   w: input vector of eigenvalues
-	// ----------------------------------------------------------------------------
-	// Return value:
-	//   0: Success
-	//  -1: Error
-	// ----------------------------------------------------------------------------
-	// Dependencies:
-	//   dsyevc3()
-	// ----------------------------------------------------------------------------
-	// Version history:
-	//   v1.1 (12 Mar 2012): Removed access to lower triangualr part of A
-	//	     (according to the documentation, only the upper triangular part needs
-	//	     to be filled)
-	//   v1.0: First released version
-	// ----------------------------------------------------------------------------
-	private int dsyevv3(double A[][], double Q[][], double[] w)
-	{
-		double norm;          // Squared norm or inverse norm of current eigenvector
-		double n0, n1;        // Norm of first and second columns of A
-		double n0tmp, n1tmp;  // "Templates" for the calculation of n0/n1 - saves a few FLOPS
-		double thresh;        // Small number used as threshold for floating point comparisons
-		double error;         // Estimated maximum roundoff error in some steps
-		double wmax;          // The eigenvalue of maximum modulus
-		double f, t;          // Intermediate storage
-		int i, j;             // Loop counters
-		
-		double DBL_EPSILON=2.2204460492503131E-16;
-		double mpmEpsilon = DBL_EPSILON;		// FLT_EPSILON or DBL_EPSILON
-		
-		wmax = Math.abs(w[0]);
-		if ((t=Math.abs(w[1])) > wmax)
-			wmax = t;
-		if ((t=Math.abs(w[2])) > wmax)
-			wmax = t;
-		double sqr = 8.0 * mpmEpsilon * wmax;
-		thresh =sqr*sqr;
-		
-		// Prepare calculation of eigenvectors
-		n0tmp   = A[0][1]*A[0][1] + A[0][2]*A[0][2];
-		n1tmp   = A[0][1]*A[0][1] + A[1][2]*A[1][2];
-		Q[0][1] = A[0][1]*A[1][2] - A[0][2]*A[1][1];
-		Q[1][1] = A[0][2]*A[0][1] - A[1][2]*A[0][0];
-		Q[2][1] = A[0][1]*A[0][1];
-		
-		// Calculate first eigenvector by the formula
-		//   v[0] = (A - w[0]).e1 x (A - w[0]).e2
-		A[0][0] -= w[0];
-		A[1][1] -= w[0];
-		Q[0][0] = Q[0][1] + A[0][2]*w[0];
-		Q[1][0] = Q[1][1] + A[1][2]*w[0];
-		Q[2][0] = A[0][0]*A[1][1] - Q[2][1];
-		norm    = Q[0][0]*Q[0][0] + Q[1][0]*Q[1][0] + Q[2][0]*Q[2][0];
-		n0      = n0tmp + A[0][0]*A[0][0];
-		n1      = n1tmp + A[1][1]*A[1][1];
-		error   = n0 * n1;
-		
-		if (n0 <= thresh)         // If the first column is zero, then (1,0,0) is an eigenvector
-		{
-			Q[0][0] = 1.0;
-			Q[1][0] = 0.0;
-			Q[2][0] = 0.0;
-		}
-		else if (n1 <= thresh)    // If the second column is zero, then (0,1,0) is an eigenvector
-		{
-			Q[0][0] = 0.0;
-			Q[1][0] = 1.0;
-			Q[2][0] = 0.0;
-		}
-		else if (norm < (64.0 * mpmEpsilon)*(64.0 * mpmEpsilon) * error)
-		{                         // If angle between A[0] and A[1] is too small, don't use
-			t = A[0][1]*A[0][1];       // cross product, but calculate v ~ (1, -A0/A1, 0)
-			f = -A[0][0] / A[0][1];
-			if (A[1][1]*A[1][1] > t)
-			{
-				t = A[1][1]*A[1][1];
-				f = -A[0][1] / A[1][1];
-			}
-			if (A[1][2]*A[1][2] > t)
-				f = -A[0][2] / A[1][2];
-			norm    = 1.0/Math.sqrt(1 + f*f);
-			Q[0][0] = norm;
-			Q[1][0] = f * norm;
-			Q[2][0] = 0.0;
-		}
-		else                      // This is the standard branch
-		{
-			norm = Math.sqrt(1.0 / norm);
-			for (j=0; j < 3; j++)
-				Q[j][0] = Q[j][0] * norm;
-		}
-		
-		// Prepare calculation of second eigenvector
-		t = w[0] - w[1];
-		if (Math.abs(t) > 8.0 * mpmEpsilon * wmax)
-		{
-			// For non-degenerate eigenvalue, calculate second eigenvector by the formula
-			//   v[1] = (A - w[1]).e1 x (A - w[1]).e2
-			A[0][0] += t;
-			A[1][1] += t;
-			Q[0][1]  = Q[0][1] + A[0][2]*w[1];
-			Q[1][1]  = Q[1][1] + A[1][2]*w[1];
-			Q[2][1]  = A[0][0]*A[1][1] - Q[2][1];
-			norm     = Q[0][1]*Q[0][1] + Q[1][1]*Q[1][1] + Q[2][1]*Q[2][1];
-			n0       = n0tmp + A[0][0]*A[0][0];
-			n1       = n1tmp + A[1][1]*A[1][1];
-			error    = n0 * n1;
-			
-			if (n0 <= thresh)       // If the first column is zero, then (1,0,0) is an eigenvector
-			{
-				Q[0][1] = 1.0;
-				Q[1][1] = 0.0;
-				Q[2][1] = 0.0;
-			}
-			else if (n1 <= thresh)  // If the second column is zero, then (0,1,0) is an eigenvector
-			{
-				Q[0][1] = 0.0;
-				Q[1][1] = 1.0;
-				Q[2][1] = 0.0;
-			}
-			else if (norm < (64.0 * mpmEpsilon)*(64.0 * mpmEpsilon) * error)
-			{                       // If angle between A[0] and A[1] is too small, don't use
-				t = A[0][1]*A[0][1];     // cross product, but calculate v ~ (1, -A0/A1, 0)
-				f = -A[0][0] / A[0][1];
-				if (A[1][1]*A[1][1] > t)
-				{
-					t = A[1][1]*A[1][1];
-					f = -A[0][1] / A[1][1];
-				}
-				if (A[1][2]*A[1][2] > t)
-					f = -A[0][2] / A[1][2];
-				norm    = 1.0/Math.sqrt(1 + f*f);
-				Q[0][1] = norm;
-				Q[1][1] = f * norm;
-				Q[2][1] = 0.0;
-			}
-			else
-			{
-				norm = Math.sqrt(1.0 / norm);
-				for (j=0; j < 3; j++)
-					Q[j][1] = Q[j][1] * norm;
-			}
-		}
-		else
-		{
-			// For degenerate eigenvalue, calculate second eigenvector according to
-			//   v[1] = v[0] x (A - w[1]).e[i]
-			//
-			// This would really get to complicated if we could not assume all of A to
-			// contain meaningful values.
-			A[1][0]  = A[0][1];
-			A[2][0]  = A[0][2];
-			A[2][1]  = A[1][2];
-			A[0][0] += w[0];
-			A[1][1] += w[0];
-			for (i=0; i < 3; i++)
-			{
-				A[i][i] -= w[1];
-				n0       = A[0][i]*A[0][i] + A[1][i]*A[1][i] + A[2][i]*A[2][i];
-				if (n0 > thresh)
-				{
-					Q[0][1]  = Q[1][0]*A[2][i] - Q[2][0]*A[1][i];
-					Q[1][1]  = Q[2][0]*A[0][i] - Q[0][0]*A[2][i];
-					Q[2][1]  = Q[0][0]*A[1][i] - Q[1][0]*A[0][i];
-					norm     = Q[0][1]*Q[0][1] + Q[1][1]*Q[1][1] + Q[2][1]*Q[2][1];
-					if (norm > (256.0 * mpmEpsilon)*(256.0 * mpmEpsilon) * n0) // Accept cross product only if the angle between
-					{                                         // the two vectors was not too small
-						norm = Math.sqrt(1.0 / norm);
-						for (j=0; j < 3; j++)
-							Q[j][1] = Q[j][1] * norm;
-						break;
-					}
-				}
-			}
-			
-			if (i == 3)    // This means that any vector orthogonal to v[0] is an EV.
-			{
-				for (j=0; j < 3; j++)
-					if (Q[j][0] != 0.0)                                   // Find nonzero element of v[0] ...
-					{                                                     // ... and swap it with the next one
-						norm          = 1.0 / Math.sqrt(Q[j][0]*Q[j][0] + Q[(j+1)%3][0]*Q[(j+1)%3][0]);
-						Q[j][1]       = Q[(j+1)%3][0] * norm;
-						Q[(j+1)%3][1] = -Q[j][0] * norm;
-						Q[(j+2)%3][1] = 0.0;
-						break;
-					}
-			}
-		}
-		
-		
-		// Calculate third eigenvector according to
-		//   v[2] = v[0] x v[1]
-		Q[0][2] = Q[1][0]*Q[2][1] - Q[2][0]*Q[1][1];
-		Q[1][2] = Q[2][0]*Q[0][1] - Q[0][0]*Q[2][1];
-		Q[2][2] = Q[0][0]*Q[1][1] - Q[1][0]*Q[0][1];
-		
-		return 0;
-	}
 
 	private int dsyevq3(double A[][], double Q[][], double w[])
 	// ----------------------------------------------------------------------------
