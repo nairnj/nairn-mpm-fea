e902be2956946efd2daa7bdfa26706b68893cc39
John A. Nairn <john@geditcom.com>
2026-03-28 09:51:36 -0700

    NairnMPM Version 19 Complete
    All items for NairnMPM version 19 are not on GitHub along with new NairnFEAMPMViz package on documentation site (a new version of Mac NairnFEAMPM will be posted soon).

--- Common/System/svninfo.cpp
+++ Common/System/svninfo.cpp
@@ -1,2 +1,2 @@
 #include "stdafx.h"
-const char *svninfo = "URL: https://github.com/nairnj/nairn-mpm-fea, Date: 2026-03-25";
+const char *svninfo = "URL: https://github.com/nairnj/nairn-mpm-fea, Date: 2026-03-28";
